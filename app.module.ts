5eab672a9937d1673a3719b2e8cf079547b1fabf
aishiekara <kmabignay@gmail.com>
2018-05-31 16:48:55 +0800

    Create app.module.ts

---
+++ app.module.ts
@@ -0,0 +1,12 @@
+import { NgModule }      from '@angular/core';
+import { AppComponent }  from './app.component';
+import { FloatThead } from './floatthead.directive';
+
+@NgModule ({
+   declarations: [ 
+     AppComponent,
+     FloatThead
+   ],
+
+})
+export class AppModule { }
