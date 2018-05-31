8a3c365daead371a1534e6a59384541c40c4d723
aishiekara <kmabignay@gmail.com>
2018-05-31 16:54:46 +0800

    Update app.component.ts

--- app.component.ts
+++ app.component.ts
@@ -2,25 +2,25 @@ import { Component } from '@angular/core';
 
 @Component ({
    selector: 'floatthead-app',
-   template: '
-   <h1>{{appTitle}}</h1>
-   <div class="container">
-     <table floatthead>
-       <thead>
-         <tr>
-           <th>Sample Header 1</th>
-           <th>Sample Header 2</th>
-         </tr>
-       </thead>
-       <tbody>
-         <tr>
-           <td>Data Col 1</td>
-           <td>Data Col 2</td>
-         </tr>
-       </tbody>
-     </table>
-   </div>
-'  
+   template: `
+     <h1>{{appTitle}}</h1>
+     <div class="container">
+       <table floatthead>
+         <thead>
+           <tr>
+             <th>Sample Header 1</th>
+             <th>Sample Header 2</th>
+           </tr>
+         </thead>
+         <tbody>
+           <tr>
+             <td>Data Col 1</td>
+             <td>Data Col 2</td>
+           </tr>
+         </tbody>
+       </table>
+     </div>
+    `  
 })
 
 export class AppComponent {
