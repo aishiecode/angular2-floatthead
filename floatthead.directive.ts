17534b3564a7f1504439b9b6521156f4ee286a19
aishiekara <kmabignay@gmail.com>
2018-05-31 16:40:56 +0800

    Create floatthead.directive.ts

---
+++ floatthead.directive.ts
@@ -0,0 +1,30 @@
+import {Directive, AfterViewInit, ElementRef} from '@angular/core';
+
+declare var jQuery: any;
+require('floatthead/dist/jquery.floatThead.js');
+
+@Directive({
+    selector: '[floatthead]'
+})
+export class FloatThead implements AfterViewInit {
+    $el: any;
+
+    constructor(private elementRef: ElementRef) {
+    }
+
+    ngAfterViewInit() {
+        this.$el = jQuery(this.elementRef.nativeElement);
+        // I used window scroll event so the floathead will only 
+        // show on window scroll with pageYOffset > 100
+        // because, floathead reflow only on window resize but not on table resize
+        window.addEventListener('scroll', (e) => {
+          if (window.pageYOffset > 100) {
+            this.$el.floatThead({
+              position: 'fixed',
+            });
+          } else {
+            this.$el.floatThead('destroy');
+          }
+        });
+    }
+}
